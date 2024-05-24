dccefa2b70e20d8d8fe4d352d15b52871c27f9ef
eric-zamora-tech <eric.zamora.career@gmail.com>
2024-05-24 11:35:51 -0500

    Working API and answer choices.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoading, setLoading] = useState(true);
+  const [countries, setCountries] = useState({list: []});
+  const answerChoices = [];
+
+  useEffect(() => {
+    getAPIData();
+  }, []);
+
+  const getAPIData = async () => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  if(isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const correctAnswer = Math.floor(Math.random() * countries.length);
+  answerChoices.push(countries[correctAnswer]);
+
+  for(let i = 0; i < 3; i++) {
+    let wrongAnswer = Math.floor(Math.random() * countries.length);
+    if(wrongAnswer == correctAnswer) {
+      i--;
+      continue;
+    }
+    answerChoices.push(countries[wrongAnswer]);
+  }
+
+  const shuffledAnswerChoices = answerChoices.sort((a, b) => 0.5 - Math.random());
+
+  function checkAnswer(answerChoice) {
+    if(answerChoice == countries[correctAnswer].name.common) {
+      console.log("correct");
+      return;
+    }
+    console.log("incorrect");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h2>Guess the flag:</h2>
+      <img src={countries[correctAnswer].flags.png}></img>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {shuffledAnswerChoices.map((choice) => <button onClick={() => checkAnswer(choice.name.common)} key={choice.cca2}>{choice.name.common}</button>)}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
